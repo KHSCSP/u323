9a9be5db1142229f75cc0f3ba839237c901ab9c2
heyscottn <heyscottn@gmail.com>
2024-04-26 10:12:46 -0500

    mm

---
+++ data_runner.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# 1, read in the data ----------------
+df = pd.read_csv("u32SOLNS/u323SOLNS/data.csv", header=0)    # header=0 means there is a header in row 0
+
+# create a list of unique 'CONTRIBUTING FACTOR VEHICLE 1'
+# TODO
+
+# print("debugging unique factors:", unique_factors)
+
+
+
+
+# 2, create a list of counts -----------------------
+# this will require a loop
+counts = []
+# TODO
+
+# print("debugging counts:", counts)
+# print("checking length, must be the same")
+# print(len(unique_factors))
+# print(len(counts))
+
+
+
+
+# 3, create a pie chart ----------------------
+plt.title('Causes of accidents between motor vehicles and bicycles')
+# TODO
+
+# plt.show()
+
---
+++ explore.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# part 1, pie chart ---------------------------------------------
+# at its most basic:
+# we create a plot, configure the options, and show it
+data = [35, 25, 25, 15]
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+# ^^ fake data
+
+# TODO
+
+
+
